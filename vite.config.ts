ba8629b4b38036f71a11d5d68f230d63f1dabb5a
yesh thakur <tyesh3651@gmail.com>
2026-07-20 00:16:08 +0530

    Rebuild after base fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My_portfolio/',
-  server: {
-    port: 3000,
-    open: true,
-  },
-})
+  base: "/My_portfolio/",
+});
